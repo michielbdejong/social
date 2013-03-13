4bfea15a6316a0b380536b80f320ff8a33199c5a
Michiel de Jong <michiel@unhosted.org>
2013-03-13 15:06:41 +0800

    fields make more sense this way

--- remotestorage/contacts.js
+++ remotestorage/contacts.js
@@ -1,17 +1,30 @@
 remoteStorage.defineModule('contacts', function(privateClient, publicClient) {
   return {
     exports: {
-      init: function() {
+      init: function () {
         privateClient.release('');
         publicClient.release('');
+        privateClient.declareType('contact', {
+          description: 'an entry in the user\'s addressbook',
+          type: 'object',
+          properties: {
+            platform: { type: 'string', description: 'Refers to a platform as defined by sockethub, for instance \'facebook\' or \'email\'', required: true },
+            name: { type: 'string', description: 'human-readable full name', required: true },
+            identifier: { type: 'string', description: 'a string that uniquely identifies this user within the platform', required: true }
+          }
+        });
       },
 
       getContacts: function(platform) {
         return privateClient.getAll(platform+'/');
       },
 
-      addContact: function(platform, address, data) {
-        return privateClient.storeObject('contact', platform+'/'+address, data);
+      addContact: function(platform, identifier, name) {
+        return privateClient.storeObject('contact', platform+'/'+identifier, {
+          platform: platform,
+          identifier: identifier,
+          name: name
+        });
       }
     }
   };
